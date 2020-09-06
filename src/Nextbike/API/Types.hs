bb3565102c3a841c384238a315f91e587e3e5015
Jonathan Strickland <djanatyn@gmail.com>
2020-09-06 10:38:04 -0400

    more typeclass instances for newtypes

--- src/Nextbike/API/Types.hs
+++ src/Nextbike/API/Types.hs
@@ -17,18 +17,20 @@ where
 
 import Data.Aeson (FromJSON (..), withObject, (.:))
 import Data.Coerce (coerce)
+import Data.String (IsString)
 import Data.Text (Text)
+import Servant.API (ToHttpApiData)
 
-newtype ApiKey = ApiKey Text deriving (Show)
+newtype ApiKey = ApiKey Text deriving (Show, ToHttpApiData, IsString) via Text
 
 instance FromJSON ApiKey where
   parseJSON = withObject "response" $ \o -> do
     key <- o .: "apiKey"
     return $ ApiKey key
 
-newtype Mobile = Mobile Text deriving (Show)
+newtype Mobile = Mobile Text deriving (Show, ToHttpApiData, IsString) via Text
 
-newtype Pin = Pin Int deriving (Show)
+newtype Pin = Pin Int deriving (Show, ToHttpApiData, Num) via Int
 
 data NextbikeLogin = NextbikeLogin
   { apiKey :: ApiKey,
